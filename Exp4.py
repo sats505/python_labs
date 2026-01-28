0bd809e0197bee122bbb21d88c40e0f78b0ad5e7
Shatakshi Upadhyay <shatakshiupadhyay05@gmail.com>
2026-01-28 11:17:08 +0530

    Implement list, tuple, for loop, and while loop examples
    Add examples of list and tuple iteration, and loops.

---
+++ Exp4.py
@@ -0,0 +1,20 @@
+my_list = [10, 20, 30, 40]
+my_tuple = (10, 20, 30, 40)
+print("List elements:")
+for item in my_list:
+    print(item)
+print("Tuple elements:")
+for item in my_tuple:
+    print(item)
+
+#4.2
+#for loop
+sequence = [1, 2, 3, 4, 5]
+for i in sequence:
+    print(i)
+
+#while loop
+num = int(input("Enter a number: "))
+while num >= 0:
+    print(num)
+    num -= 1
