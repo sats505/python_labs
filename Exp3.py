3784f7e6133df526d6826049152000d7ccc81fd4
Shatakshi Upadhyay <shatakshiupadhyay05@gmail.com>
2026-01-28 11:15:36 +0530

    Add number parity check and fraction printing

---
+++ Exp3.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
+
+#3.2
+for i in range(2, 11):
+    print("1/", i, "=", 1/i)
